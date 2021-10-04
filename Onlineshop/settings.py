048e9ce78d435c2cf87938e79ac0b59fe05cea10
trung03trung <89577344+trung03trung@users.noreply.github.com>
2021-10-04 15:48:28 +0700

    first commit

--- Onlineshop/settings.py
+++ Onlineshop/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+from os import environ
+SECRET_KEY = environ['secret_key']
+import django_heroku
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
